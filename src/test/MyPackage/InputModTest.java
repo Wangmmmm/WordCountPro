2e421a1366f3d8b6e9fbddc7d55fa8c49eaa4904
HuangDongPeng <536762250@qq.cmom>
2018-04-04 15:24:59 +0800

    add test class

---
+++ src/test/MyPackage/InputModTest.java
@@ -0,0 +1,65 @@
+package test.MyPackage; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* InputMod Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 4, 2018</pre> 
+* @version 1.0 
+*/ 
+public class InputModTest { 
+
+@Before
+public void before() throws Exception {
+}
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: ReadContent(String path) 
+* 
+*/ 
+@Test
+public void testReadContent() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: IsTxtFormat(String path) 
+* 
+*/ 
+@Test
+public void testIsTxtFormat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: IsAbsolutePath(String path) 
+* 
+*/ 
+@Test
+public void testIsAbsolutePath() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: ShowWordTable() 
+* 
+*/ 
+@Test
+public void testShowWordTable() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
