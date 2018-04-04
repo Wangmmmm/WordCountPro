7cf645ff14165bc47002a6bb62e6e5a053cebd5b
wmmmm <1229819460@qq.com>
2018-04-04 10:40:21 +0800

    排序功能实现

--- src/MyString.java
+++ src/MyString.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class MyString {
+public class MyString  implements Comparable{
 
 
     public String string;
@@ -9,25 +9,92 @@ public class MyString {
         string =s;
         count=1;
     }
+    public MyString(String s,int n)
+    {
+        string =s;
+        count=n;
+    }
+    public MyString(MyString ms)
+    {
+        this.string =ms.string;
+        this.count=ms.count;
+    }
+
 
     public int GetCount()
     {
         return count;
     }
+    public int compareTo(Object o) {
+        MyString s = (MyString) o;
+        if (this.count > s.count) {
+            return 1;
+        }
+        else if(this.count==s.count)
+        {
+            if(this.string.compareTo(s.string)<0)
+            {
+                return 1;
+            }
+            else if((this.string.compareTo(s.string)==0))return 0;
+            else return -1;
+        }
+        else {
+            return -1;
+        }
+      //  return 0;
+    }
 
     private static ArrayList<MyString> myStrings=new ArrayList<MyString>();
     public static void  Insert(String s)
     {
-            if(myStrings.isEmpty()){myStrings.add(new MyString(s));return;}
+        if(myStrings.isEmpty()){myStrings.add(new MyString(s));return;}
+        boolean find=false;
         for (MyString ms:myStrings )
         {
-            if(ms.string.equals(s))ms.count++;
-            else myStrings.add(new MyString(s));
+            if(ms.string.equals(s)){ms.count++;find=true;}
         }
+        if(!find) myStrings.add(new MyString(s));
     }
     public static ArrayList<MyString> GetStrings()
     {
-        return null;
+        quickSort(myStrings,0,myStrings.size()-1);
+        if(myStrings.size()<=100)return myStrings;
+        else return (ArrayList<MyString>) myStrings.subList(0,99);
+       // return null;
+    }
+
+
+    private static int partition(ArrayList<MyString> array,int lo,int hi){
+        /** 固定的切分方式 */
+        MyString key=new MyString(array.get(lo)) ;//选取了基准点
+        while(lo<hi){
+            //从后半部分向前扫描
+            while(array.get(hi).compareTo(key)<=0&&hi>lo){
+                hi--;
+            }
+            array.set(lo,new MyString(array.get(hi)));
+            //从前半部分向后扫描
+            while(array.get(lo).compareTo(key)>=0&&hi>lo){
+                lo++;
+            }
+            array.set(hi,new MyString(array.get(lo)));
+        }
+        array.set(hi,new MyString(key));
+        //array[hi]=key;//最后把基准存入
+        return hi;
+    }
+
+    private static void quickSort(ArrayList<MyString> array,int lo ,int hi){
+        if(lo>=hi){
+            return ;
+        }
+        //进行第一轮排序获取分割点
+        int index=partition(array,lo,hi);
+        //排序前半部分
+        quickSort(array, lo, index - 1);
+        //排序后半部分
+        quickSort(array,index+1,hi);
     }
 
 
--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,30 @@
+import java.util.ArrayList;
+
 public class Program {
     public static void main(String[] args)
     {
 
-        String s="asd";
-        String b=s;
-        s="asds";
-        System.out.print(b==s);
+
+
+        Statistics st=new Statistics();
+        String s="asdssssss,dh,123as-dfhh.dh'";
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            st.Count(c);
+        }
+        st.End();
+        
+        ArrayList<MyString> mss=MyString.GetStrings();
+
+
+        for (MyString stemp:mss
+                ) {
+            System.out.println(stemp.GetCount()+" "+stemp.string);
+        }
+
+        //System.out.pri ntln(a);
+
     }
 
 }
